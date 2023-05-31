dbe76093079acda75265a8228f2cf02e923c3a58
Peter Kerins <pete.kerins@gmail.com>
2023-05-31 17:09:27 +0100

    update packages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
+// import './App.css'
 import React, { useState } from 'react'
-import './App.css'
 import ConvertString from './convertString'
 
 function App() {
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
+// import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export default function NextIndexWrapper() {
+  return <App />;
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
